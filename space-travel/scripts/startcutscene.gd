8dbdd1e9d5e59169cc192ad7ac58517e808869e0
Fishyyyyyyyyy <justvishy.v@gmail.com>
2026-06-02 19:01:35 -0500

    Finished cutscene and load logic
    The cutscene is finished and the loading screen pops up, now users can choose to restart or load their current game

--- space-travel/scripts/startcutscene.gd
+++ space-travel/scripts/startcutscene.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 
 func _ready() -> void:
+	$AnimatedSprite2D.frame = 1
+	$Burntearth.visible = false
 	$AnimatedSprite2D2.visible = false
 	$AnimatedSprite2D2.play("default")
 	$AnimatedSprite2D.play("default")
@@ -12,8 +14,17 @@ func _ready() -> void:
 	$AnimatedSprite2D2.visible = true
 	$AnimationPlayer.play("spaceshipfire")
 	await $AnimationPlayer.animation_finished
+	$Burntearth.visible = true
 	$loadmenu.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_restart_pressed() -> void:
+	pass # Replace with function body.
